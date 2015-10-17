c65a98e93d2ce951018624e4df34e293534002ec
sojufrank <frankyiyi@gmail.com>
2015-10-16 23:41:06 -0700

    updated json

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem 'responders'
 gem 'httparty'
+gem 'crack'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/crime_controller.rb
+++ app/controllers/crime_controller.rb
@@ -1,22 +1,32 @@
 require 'rubygems'
 require 'httparty'
+require 'crack'
 class CrimeController < ApplicationController
 
-	def home
+    def home
 
-	end
+    end
+
  
   def crimedata
     @crime = HTTParty.get("https://data.seattle.gov/resource/7ais-f98f.json")
 
-    render json: @crime
+    @hash = JSON.parse(@crime.body)
 
-    #respond_to do |format|
-    	#format.html
-    	#format.json { render json: @crime.as_json(only: [:location]) }
-    #end
+    #puts hash
+
+    #render json: hash
+
+    #my_hash_array = Crack::JSON.parse(@crime.body)
+    #puts my_hash_array
+
+    respond_to do |format|
+        format.html
+        format.json { render json: @crime }
+    end
     #puts @crime.body, @crime.code, @crime.message, @crime.headers.inspect
     #puts @crime
   end
 
-end
+end 
+

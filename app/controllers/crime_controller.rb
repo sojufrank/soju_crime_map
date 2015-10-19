536acb622da1856571692af652057dca3b373462
sojufrank <frankyiyi@gmail.com>
2015-10-18 20:00:09 -0700

    updated with bootstrap gem

--- app/controllers/crime_controller.rb
+++ app/controllers/crime_controller.rb
@@ -5,42 +5,9 @@ class CrimeController < ApplicationController
     end
 
  
-  def crimedata
-    
-    #@crime = HTTParty.get("https://data.seattle.gov/resource/7ais-f98f.json")
+    def crimedata
 
-    #@hash = JSON.parse(@crime.body)
-
-    #@soju = @crime
-
-   # @soju1 = JSON.pretty_generate(@soju)
-
-    #puts '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-  #  puts @soju.first
-
-
-
-    #@soju.to_json
-   
-
-
-    #@hash.to_json
-
-    #puts hash
-
-    #render json: hash
-
-    #my_hash_array = Crack::JSON.parse(@crime.body)
-    #puts my_hash_array
-
-    #respond_to do |format|
-    #    format.html
-    #    format.json { render json: @soju }
-    #end
-    #puts @crime.body, @crime.code, @crime.message, @crime.headers.inspect
-    #puts @crime
-    
-  end
+    end
 
 end 
 
